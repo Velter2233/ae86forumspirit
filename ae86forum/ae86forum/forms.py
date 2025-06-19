e6390eed21d3f5ddd77df797a4601335296f3346
Velter2233 <deman222333@gmail.com>
2025-06-19 21:38:48 +0300

    выгрузка

--- ae86forum/ae86forum/forms.py
+++ ae86forum/ae86forum/forms.py
@@ -1,14 +1,8 @@
-from django import forms
 from spirit.user.auth.forms import RegistrationForm
 from django_recaptcha.fields import ReCaptchaField
-from django_recaptcha.widgets import ReCaptchaV2Checkbox  # или V2Invisible, если надо
+from django_recaptcha.widgets import ReCaptchaV2Checkbox
 from django.conf import settings
 
 
 class CustomRegistrationForm(RegistrationForm):
-    recaptcha = ReCaptchaField(
-        widget=ReCaptchaV2Checkbox,
-        public_key=settings.RECAPTCHA_PUBLIC_KEY,
-        private_key=settings.RECAPTCHA_PRIVATE_KEY,
-        label='Подтвердите, что вы не робот'
-    )
+    recaptcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
